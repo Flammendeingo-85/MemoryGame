f003d84acffb64485af59d67836011fd41f73f15
Flammendeingo-85 <Ravi.Banwari1108@gmail.com>
2022-11-10 16:17:26 +0100

    added some styling to card.css

--- memory-game/src/components/card.jsx
+++ memory-game/src/components/card.jsx
@@ -1,5 +1,14 @@
+import head from "../assets/head.svg";
+
 const Card = ({ emoji }) => {
-  return <div className="card">{emoji}</div>;
+  return <div className="card-container">
+    <div className="front">
+      <img src={head} alt="head" width="60" />
+    </div>
+    <div className="back">
+      {emoji}
+    </div>
+  </div>;
 };
 
 export default Card;
