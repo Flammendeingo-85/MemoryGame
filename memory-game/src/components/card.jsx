aee9cc80d1114f07b8cabfedc5e7025e8b248474
Flammendeingo-85 <Ravi.Banwari1108@gmail.com>
2022-11-12 12:52:48 +0100

    committing to save last edit

--- memory-game/src/components/card.jsx
+++ memory-game/src/components/card.jsx
@@ -1,14 +1,18 @@
 import head from "../assets/head.svg";
 
 const Card = ({ emoji }) => {
-  return <div className="card-container">
-    <div className="front">
-      <img src={head} alt="head" width="60" />
-    </div>
-    <div className="back">
-      {emoji}
-    </div>
-  </div>;
+  function handleCardClick() {
+    console.log("click");
+  }
+
+  return (
+    <button className="card-container" onClick={handleCardClick}>
+      <div className="front">
+        <img src={head} alt="head" width="60" />
+      </div>
+      <div className="back">{emoji}</div>
+    </button>
+  );
 };
 
 export default Card;
